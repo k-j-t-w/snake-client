f74afea3e1477d283357b1de07995dd6d6309450
Kai Wingfield <kaiwingfield17@gmail.com>
2023-12-13 09:52:44 -0800

    implements functions: setupInput and handleUserInputs

--- play.js
+++ play.js
@@ -1,5 +1,23 @@
 
 const {connect} = require('./client.js')
 
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  };
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
+setupInput();
