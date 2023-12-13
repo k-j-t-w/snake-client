9c16b74fb2cf917531d70cb6f7fe133a2c94eb54
Kai Wingfield <kaiwingfield17@gmail.com>
2023-12-13 09:57:53 -0800

    refactors new functions into their file (input.js)

--- play.js
+++ play.js
@@ -1,22 +1,5 @@
-
-const {connect} = require('./client.js')
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  };
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
+const { connect } = require('./client.js')
+const { setupInput } = require('./input.js')
 
 console.log("Connecting ...");
 connect();
