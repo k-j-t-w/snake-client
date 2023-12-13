800468ab9a4bd07d725123580907a3ac23cb4fef
Kai Wingfield <kaiwingfield17@gmail.com>
2023-12-13 09:20:07 -0800

    seperates connect function to it's own .js file

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-const net = require("net");
 
-
-// establishes a connection with the game server
-const connect = function () {
-
-  const conn = net.createConnection({
-    host: '172.18.193.127',
-    port: '50541'
-  });
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
+const {connect} = require('./client.js')
 
 console.log("Connecting ...");
 connect();
